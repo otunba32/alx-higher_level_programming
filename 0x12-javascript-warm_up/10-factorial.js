e8e497cdd3add6fcd8fd3d873371c5961dc5012c
otunba32 <emmanuel.abia32@gmail.com>
2023-04-13 16:22:33 -0700

    javascript

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,13 +3,13 @@
 const num = parseInt(process.argv[2]);
 
 if (!isNaN(num)) {
-    console.log(factorial(num));
+  console.log(factorial(num));
 } else {
-	console.log(1);
+  console.log(1);
 }
-function factorial(num) {
-    if(num === 0) {
-	  return(1);
-    }
-	return num * factorial(num - 1);
+function factorial (num) {
+  if (num === 0) {
+	  return (1);
+  }
+  return num * factorial(num - 1);
 }
