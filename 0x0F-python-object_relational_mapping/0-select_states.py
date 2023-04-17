cbea953dafca5af400f3bfbf707a97276144cc4b
otunba32 <emmanuel.abia32@gmail.com>
2023-04-17 09:50:00 -0700

    ORM

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user="root", passwd="", bd="hbtn_0e_0_usa")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
