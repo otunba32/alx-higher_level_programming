0b6e6b9d6d15c1879788857c231015c00a820703
otunba32 <emmanuel.abia32@gmail.com>
2023-04-18 05:30:40 -0700

    sql

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""script that lists all states with a name startingwith N from the database hbtn_0e_0_usa"""
-import MySQLdb 
+"""
+    script that lists all states with a name starting
+    with N from the database hbtn_0e_0_usa
+"""
+import MySQLdb
 import sys
 
 if __name__ == '__main__':
@@ -11,16 +14,15 @@ if __name__ == '__main__':
             passwd=sys.argv[2],
             db=sys.argv[3],
             )
-    
+
     cur = db.cursor()
     myQuery = "".join([
             "SELECT * FROM states WHERE name LIKE BINARY 'N%'",
             "ORDER BY id ASC"])
     cur.execute(myQuery)
     curs = cur.fetchall()
-
     for rows in curs:
         print(rows)
-        
+
     cur.close()
     db.close()
