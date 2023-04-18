2dbcafea66309fac6c360b71fc3132fce8d7fcf5
otunba32 <emmanuel.abia32@gmail.com>
2023-04-18 06:22:19 -0700

    sql

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""a script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            )
+    cur = db.cursor()
+    myQuery = "".join([
+        "SELECT * FROM states WHERE name LIKE BINARY '{}'",
+        "ORDER BY id ASC",
+        ]).format(sys.argv[4])
+    cur.execute(myQuery)
+    curs = cur.fetchall()
+
+    for rows in curs:
+        print(curs)
+
+    cur.close()
+    db.close()
