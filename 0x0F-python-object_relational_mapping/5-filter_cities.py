6bf3aecd9cc1a6ccf09a4b5045b4fdd623dc72ab
otunba32 <emmanuel.abia32@gmail.com>
2023-04-21 05:30:49 -0700

    orm

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            )
+    cur = db.cursor()
+    myQuery = " ".join([
+        "SELECT cities.name FROM cities",
+        "INNER JOIN states ON states.id = cities.state_id",
+        "WHERE states.name LIKE BINARY '{}'",
+        "ORDER BY cities.id",
+        ]).format(sys.argv[4])
+    cur.execute(myQuery)
+    res = cur.fetchall()
+    strRes = ', '.join([w[0] for w in res])
+    print(strRes)
+    cur.close()
+    db.close()
