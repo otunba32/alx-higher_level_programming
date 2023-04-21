af273f952862ffbb5d818c0c319f6029dea632bc
otunba32 <emmanuel.abia32@gmail.com>
2023-04-21 07:17:28 -0700

    orm

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that contains the class definition of city and instance Base"""
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+class City(Base):
+    """City class that inherits from Base"""
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, unique=True,
+            nullable=False, primary_key=True)
+    name = Column(string(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a file that contains the class definition of a State and an instance Base"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class State(Base):
+    """State class that inherits from Base declarative"""
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, unique=True,
+            nullable=False, primary_key=True)
+
+    name = Column(String(128), nullable=False)
